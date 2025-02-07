f5f84991e84038ce9e0bf82a397bc136afbdef07
MatteoFabianiMeucci <matteofabiani06@gmail.com>
2025-02-07 10:31:15 +0100

    Setup iniziale

---
+++ src/main/java/com/Time.java
@@ -0,0 +1,49 @@
+package com;
+
+public class Time {
+    protected int ore;
+    protected int minuti;
+    protected int secondi;
+
+    public Time(int secondi){
+
+    }
+
+    public Time(int ore, int minuti, int secondi){
+
+    }
+
+    public int getOre(){
+
+    }
+
+    public int getMinuti(){
+
+    }
+    public int getSecondi(){
+
+    }
+
+    public void setOre(int ore){
+
+    }
+
+    public void setMinuti(int minuti){
+
+    }
+    public void setSecondi(int secondi){
+
+    }
+
+    public void addSecondi(int secondi){
+
+    }
+
+    public int convertiSecondi(){
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
